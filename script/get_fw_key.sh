2f4393d4b20348797156754285e6bfa9c4431baf
dma@paloaltonetworks.com <dma@paloaltonetworks.com>
2020-09-23 12:18:10 +0800

    updated the script

--- script/get_fw_key.sh
+++ script/get_fw_key.sh
@@ -3,8 +3,12 @@ set -e
 eval "$(jq -r '@sh "export EIP=\(.eip) USERNAME=\(.username) PASSWORD=\(.password)"')"
 
 function produce_output() {
-  export api=$(curl -k https://"${EIP}"/api/\?type\=keygen\&user\="${USERNAME}"\&password\="${PASSWORD}" | awk -F"<key>" '{print $2}' | awk -F"</key>" '{print $1}')
-
+  api=""
+  while [[ -z "$api" ]]; do
+  	export api=$(curl -k https://"${EIP}"/api/\?type\=keygen\&user\="${USERNAME}"\&password\="${PASSWORD}" | awk -F"<key>" '{print $2}' | awk -F"</key>" '{print $1}')
+    sleep 30
+  done
+  
   jq -n \
     --arg api_key "$api" \
     '{"api_key":$api_key}'
